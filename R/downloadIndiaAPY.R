7ad186ac0f3fc849eafc5289129919930591652a
Anastasis Giannousakis <11047746+giannou@users.noreply.github.com>
2020-11-05 18:46:24 +0100

    added all crops and simplified the read function

--- R/downloadIndiaAPY.R
+++ R/downloadIndiaAPY.R
@@ -9,8 +9,10 @@
 downloadIndiaAPY <- function() {
 
   url <- "https://eands.dacnet.nic.in/"
-  crops <- c("Rice","Wheat","Maize","Bajra","Barley")
   years <- c("APY_state_data/Apy-1966-76/Foodgrains/","Archive/Year76-86/","10-Year-1985-96/")
+  b<-suppressWarnings(readLines("https://eands.dacnet.nic.in/StateData_66-76Year.htm"))
+  crops <- gsub("\\..*.","",gsub(".*.Foodgrains/","",grep("APY.*.Food.*.xls",b,value=TRUE))) # extract filenames
+  crops <- grep(" |Summary",crops,value=TRUE,invert = T) # keep only single names (the rest are not relevant)
   for (i in crops) {
     for (j in years) {
       for (k in c(".xls", ".xlsx")) {
@@ -20,6 +22,7 @@ downloadIndiaAPY <- function() {
   }
   suppressWarnings(try(download.file("https://eands.dacnet.nic.in/PDF/foodgrain-5_years.xls",destfile = "allfood1996-2013.xls")))
   suppressWarnings(try(download.file("https://eands.dacnet.nic.in/PDF/5-Year%20Foodgrain%202018-19.xls",destfile = "allfood2014-2018.xls")))
+  writeLines(crops,con = "crops.txt")
   
 }
  
