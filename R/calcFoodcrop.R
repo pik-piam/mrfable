8310ca856c0ab6d428f0fee0bbe4dc982a77f56f
giannou <11047746+giannou@users.noreply.github.com>
2020-11-17 20:19:49 +0100

    fixed and renamed calc function

---
+++ R/calcFoodcrop.R
@@ -0,0 +1,23 @@
+#' @title calcFoodcrop
+#' Calculates foodcrops data for India taken from here: https://eands.dacnet.nic.in/APY_96_To_07.htm
+#' @author Anastasis Giannousakis
+#' @param subtype Area, Yield, or Production
+#' @importFrom madrat readSource toolAggregate
+#' @importFrom magclass getNames
+#' @importFrom utils read.csv
+#' @examples
+#' \dontrun{ a <- madrat::calcOutput("Foodcrop",subtype="Area") }
+#' @return magpie object containing Area, Yield, and Production data. 
+
+
+calcFoodcrop <- function(subtype="Rice"){
+  
+  x <- readSource("IndiaAPY", convert = "onlycorrect")
+
+  return(list(x=x[,,subtype],
+              weight=NULL, # TODO: adjust weights according to each variable
+              unit=getNames(x,fulldim = TRUE)$unit[which(getNames(x,fulldim = TRUE)$variable==subtype)],
+              isocountries = FALSE,
+              description=paste0("IndiaAPY ",subtype," data from: https://eands.dacnet.nic.in/APY_96_To_07.htm")))
+
+}
