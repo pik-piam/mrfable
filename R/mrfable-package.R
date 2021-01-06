1dd639644655efb569a30783b2b764d39d1c09f8
Anastasis Giannousakis <11047746+giannou@users.noreply.github.com>
2021-01-06 17:02:38 +0100

    add help pages

---
+++ R/mrfable-package.R
@@ -0,0 +1,11 @@
+
+#' The mrfable R package (runs with madrat)
+#'
+#' Provides functions to read in FABLE project data for India from here: https://eands.dacnet.nic.in/APY_96_To_07.htm
+#'
+#' @name mrfable-package
+#' @aliases mrfable-package mrfable
+#' @docType package
+#' @author Anastasis Giannousakis
+#'
+"_PACKAGE"
